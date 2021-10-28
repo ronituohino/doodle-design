5204e6532e35ae608c4e9baf36b514a40cc76713
Roni Tuohino <tuohinoroni@gmail.com>
2021-10-28 19:40:15 +0300

    Add styling, animation and links to content!

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,11 @@
-import { Box, Container, Card, CardMedia, Typography } from "@mui/material"
+import { Box, Container, Typography } from "@mui/material"
+
+import { Link } from "react-router-dom"
 
 import { useQuery } from "@apollo/client"
 import { GET_ITEMS } from "./queries/queries"
+
+import { useSpring, animated } from "react-spring"
 
 const Content = () => {
   const { data } = useQuery(GET_ITEMS, {
@@ -33,60 +37,83 @@ const Content = () => {
 }
 
 const ItemCard = ({ item }) => {
+  const [{ blurSpread }, api] = useSpring(() => ({ blurSpread: [12, 2] }))
+
   return (
     <>
-      <Box
-        style={{
-          width: "200px",
-          height: "250px",
-          margin: 2,
-        }}
+      <Link
+        to={`/product/${item.category.toLowerCase()}/${item.name.toLowerCase()}`}
+        style={{ textDecoration: "none" }}
       >
-        <Card
-          elevation={4}
-          sx={{
-            margin: "auto",
-            width: "190px",
-            height: "190px",
-            borderRadius: 4,
-            marginBottom: "-6px",
-          }}
-        >
-          <CardMedia
-            component="img"
-            image="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-            alt="name"
-          />
-        </Card>
-        <Box
+        <animated.div
+          onMouseEnter={() =>
+            api.start({
+              to: { blurSpread: [16, 8] },
+              config: { tension: 210, friction: 20 },
+            })
+          }
+          onMouseLeave={() =>
+            api.start({
+              to: { blurSpread: [12, 2] },
+              config: { tension: 210, friction: 20 },
+            })
+          }
           style={{
-            textAlign: "center",
+            width: "200px",
+            height: "280px",
+            margin: 8,
+            borderRadius: 16,
+            boxShadow: blurSpread.to(
+              (blur, spread) =>
+                `0px 0px ${blur}px ${spread}px rgba(0, 0, 0, 0.2)`
+            ),
           }}
         >
-          <Typography
-            variant="subtitle1"
+          <img
+            component="img"
+            src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+            alt="name"
+            style={{
+              margin: "auto",
+              width: "200px",
+              height: "200px",
+              marginBottom: "-6px",
+              borderRadius: 16,
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0,
+            }}
+          />
+
+          <Box
             sx={{
-              marginTop: "10px",
-              marginBottom: "-5px",
-              textTransform: "uppercase",
-              fontWeight: "bold",
-              letterSpacing: 0.8,
+              textAlign: "center",
+              marginTop: 2.1,
             }}
           >
-            {item.name}
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            sx={{
-              textTransform: "uppercase",
-              fontWeight: "bold",
-              letterSpacing: 0.8,
-            }}
-          >
-            {item.price}
-          </Typography>
-        </Box>
-      </Box>
+            <Typography
+              variant="subtitle1"
+              color="black"
+              sx={{
+                marginBottom: "-5px",
+                fontWeight: "bold",
+                letterSpacing: 0.5,
+              }}
+            >
+              {item.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="black"
+              sx={{
+                fontWeight: "bold",
+                letterSpacing: 0.5,
+              }}
+            >
+              {item.price}
+            </Typography>
+          </Box>
+        </animated.div>
+      </Link>
     </>
   )
 }
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -6,6 +6,7 @@ export const GET_ITEMS = gql`
       id
       name(language: $language)
       price
+      category
     }
   }
 `
